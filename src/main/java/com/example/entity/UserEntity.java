da3b6e3b9e7ae4d76abf0423f39440d6251d9423
Leevanni Goncalves <leevannigoncalves@gmail.com>
2026-05-15 15:53:01 -0400

    created UserEntity and created one to many relationship with transactions

--- src/main/java/com/example/entity/UserEntity.java
+++ src/main/java/com/example/entity/UserEntity.java
@@ -3,36 +3,33 @@ package com.example.entity;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class TransactionEntity {
-
+public class UserEntity {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private BigDecimal amount;
-	private LocalDate date;
-	private String description;
-	private String category;
-	@ManyToOne
-	@JoinColumn(name = "user_id")
-	private UserEntity user;
-	private LocalDateTime updatedAt;
+	long id;
+	String userName;
+	String email;
+	long password;
+	String role;
+	@OneToMany(mappedBy = "user")
+	private List<TransactionEntity> transactions;
 
 }
